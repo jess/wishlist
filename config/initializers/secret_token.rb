374c69b4fefa1b8ce754b63a2eb9549800aa3840
Jess Brown <jess@brownwebdesign.com>
2011-11-27 16:21:51 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wishlist::Application.config.secret_token = '601ed6455b1eb2409029954367c4c566efc3b7f722199926cd50c8c0f48bddcde9be09ed673ce1ddd3e58ab67c235d1585dda5de959e0c501bdd629443724eda'
